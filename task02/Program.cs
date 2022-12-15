51682dad2deeac32fa8eecc13a9a59ece4605b14
Пользователь <mintayeva01@mail.ru>
2022-12-15 21:42:03 +0600

    added file with second task

---
+++ task02/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+//Строки использовать нельзя
+
+Console.WriteLine("Enter the number:");
+int entered_number = Convert.ToInt32(Console.ReadLine());
+int n = entered_number; 
+int sum = 0;
+while (n > 0) 
+{
+    int digit = n % 10; 
+    sum = sum + digit; 
+    n = n / 10; 
+}
+
+Console.WriteLine($"Сумма: {sum}");
